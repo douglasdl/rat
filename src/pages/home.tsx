a566c780e2545e8a58c0276eb4f0f3c35f532887
douglasdl <douglas_san@hotmail.com>
2024-09-11 14:20:54 -0300

    feat: add Prisma and Supabase

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,16 +1,40 @@
+import { useEffect, useState } from 'react'
 import ratWheel from '../assets/rat-wheel.png'
+import { supabase } from '../libs/supabaseClient'
+import Auth from '../Auth'
+import Account from '../Account'
+
+
 
 export function Home() {
+  const [barbershops, setBarbershops] = useState([]);
+
+  async function getBarbershops() {
+    const { data } = await supabase.from("Barbershop").select();
+    setBarbershops(data);
+  }
+
+  useEffect(() => {
+    getBarbershops();
+  }, []);
+
+
   return (
-    <div className="bg-green-900 flex flex-1 h-screen w-screen items-start justify-center p-4 text-white">
-      <header className='flex gap-8'>
-        <div>
+    <div className="flex-col bg-green-900 flex flex-1 gap-4 h-screen w-screen items-start justify-center p-4 text-white">
+      <header className='flex gap-8 mx-auto'>
+        <div className=''>
           <h1 className="text-3xl font-bold">Rat Gamma</h1>
           <h2>Saia da Corrida dos Ratos</h2>
         </div>
         <img src={ratWheel} alt="Rat Wheek" className='w-16 h-16 rounded-lg' />
-
       </header>
+      <main className='flex flex-1 px-4'>
+        <ul>
+          {barbershops.map((barbershop) => (
+            <li key={barbershop.name}>{barbershop.name}</li>
+          ))}
+        </ul>
+      </main>
     </div>
   )
 }
